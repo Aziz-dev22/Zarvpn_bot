36bfdf370f9995a87d64c5a347cd5ae8c9b19102
Aziz-dev22 <testnetmelli@gmail.com>
2026-06-27 23:48:20 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# لود کردن فایل تنظیمات محیطی .env
 load_dotenv()
 
-# خواندن اطلاعات از متغیرهای محیطی سرور
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
-# بررسی حیاتی بودن اطلاعات
-if not TELEGRAM_TOKEN or not ADMIN_ID:
-    raise ValueError("خطا: متغیرهای TELEGRAM_TOKEN و ADMIN_ID تنظیم نشده‌اند!")
-
-try:
-    ADMIN_ID = int(ADMIN_ID)
-except ValueError:
-    raise ValueError("خطا: ADMIN_ID باید یک عدد چندرقمی باشد!")
-
-# تنظیمات پیش‌فرض پنل مرزبان یا X-UI
-PANEL_URL = os.getenv("PANEL_URL", "https://your-panel.com:8000")
+# آدرس و مشخصات پنل مرزبان کاربر
+PANEL_URL = os.getenv("PANEL_URL", "http://YOUR_PANEL_IP:8000")
 PANEL_USERNAME = os.getenv("PANEL_USERNAME", "admin")
 PANEL_PASSWORD = os.getenv("PANEL_PASSWORD", "password")
